4122dd609f39ea4fafe071b698fcfbd299a47e5c
Kaltist <3270596564@qq.com>
2024-11-28 15:09:38 +0800

    店铺营业状态设置功能开发，通过redis设置status的参数

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,43 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("adminShopController")
+@RequestMapping("/admin/shop")
+@Api(tags="店铺相关接口")
+@Slf4j
+public class ShopController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+    /**
+     * 设置店铺营业状态
+     * @param status
+     * @return
+     */
+    @PutMapping("/{status}")
+    @ApiOperation("设置店铺营业状态")
+    public Result setStatus(@PathVariable Integer status){
+        log.info("设置店铺营业状态:{}",status==1?"营业中":"未营业");
+        redisTemplate.opsForValue().set("shopStatus",status);
+        return Result.success();
+    }
+
+    /**
+     * 查询店铺营业状态
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("查询店铺营业状态")
+    public Result<Integer> getStatus(){
+        Integer status=(Integer)redisTemplate.opsForValue().get("shopStatus");
+        log.info("查询店铺营业状态:{}",status==1?"营业中":"未营业");
+        return Result.success(status);
+    }
+}
